38497e75b971c44600a9b13a5098300128eec8be
Kamran Nazir <kamrannazir901@gmail.com>
2025-07-08 23:39:22 +0500

    pages set

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: "/udemy-ui-clone/",
+  plugins: [react(), tailwindcss(), ghPages()],
   resolve: {
     alias: {
       assets: path.resolve(__dirname, "src/assets"),
