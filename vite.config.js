2df3a98fbe8d4a4d73a8cd71708542afab6a91b2
Muhammad Kamran <91570535+kamrannazir901@users.noreply.github.com>
2025-09-17 15:52:32 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-  base: "/",
-  plugins: [react(), tailwindcss(), ghPages()],
+  base: "/", // make sure assets resolve correctly
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       assets: path.resolve(__dirname, "src/assets"),
